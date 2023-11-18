106025a60660c9ab9223602a766464686559c128
Anurag Asthana <a.asthana90@gmail.com>
2023-11-18 22:30:16 +0530

    adding service controller

--- Carsties/src/AuctionServiceNew/Program.cs
+++ Carsties/src/AuctionServiceNew/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
 {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -28,5 +29,14 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    DbInit.Init(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
 
 app.Run();
