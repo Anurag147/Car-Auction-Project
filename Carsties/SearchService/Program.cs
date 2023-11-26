55b383016165fc0ea78c7052e10ad14cf92cce26
Anurag Asthana <a.asthana90@gmail.com>
2023-11-26 19:44:31 +0530

    adding rabbit mq

--- Carsties/SearchService/Program.cs
+++ Carsties/SearchService/Program.cs
@@ -1,4 +1,6 @@
 using System.Net;
+using MassTransit;
+
 using Polly;
 using Polly.Extensions.Http;
 using SearchService;
@@ -11,6 +13,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
--- Carsties/src/AuctionServiceNew/Program.cs
+++ Carsties/src/AuctionServiceNew/Program.cs
@@ -1,4 +1,7 @@
 using AuctionServiceNew.Data;
+
+using MassTransit;
+
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +14,13 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
