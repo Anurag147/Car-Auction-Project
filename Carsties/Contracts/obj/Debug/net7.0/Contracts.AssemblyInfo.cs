ad8c482c27fe8277457a32df0c2cfd5a9946e33e
Anurag Asthana <a.asthana90@gmail.com>
2023-11-26 20:22:41 +0530

    adding contracts model

--- Carsties/Contracts/obj/Debug/net7.0/Contracts.AssemblyInfo.cs
+++ Carsties/Contracts/obj/Debug/net7.0/Contracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9551d2b4969d954be3b6d53a17c341e0c9abec00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
